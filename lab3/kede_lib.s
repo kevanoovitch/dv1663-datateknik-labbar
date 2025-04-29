78d5e0d93896ec6c11cef359a8b39b036cb05e7a
kevanoovitch <kiwikev00@gmail.com>
2025-04-29 14:08:52 +0200

    Make:able implementation

---
+++ lab3/kede_lib.s
@@ -0,0 +1,52 @@
+.text 
+.global inImage, getInt, getText, getInPos, setInPos, outImage, putInt, putText, putChar, getOutPos, setOutPos
+
+inImage:
+// TODO inplementera mer än return void
+    ret
+
+getInt:
+// TODO inplementera mer än ret 0
+    movq $0, %rax
+    ret
+
+getText:
+// TODO inplementera mer än ret 0
+    movq $0, %rax
+    ret
+
+getInPos:
+// TODO inplementera mer än ret 0
+    movq $0, %rax
+    ret
+
+setInPos:
+// TODO inplementera mer än return void
+    ret
+
+outImage:
+// TODO inplementera mer än return void
+    ret
+
+putInt:
+// TODO inplementera mer än return void
+    ret 
+
+putText:
+// TODO inplementera mer än return void
+    ret
+
+putChar:
+// TODO inplementera mer än return void
+    ret 
+
+getOutPos:
+// TODO inplementera mer än ret 0
+    movq $0, %rax
+    ret
+
+setOutPos:
+// TODO inplementera mer än return void
+    ret
+
+
